a6dd97ae3932a863585c0e84ce8c1c54c4af5258
jcroomgitr <44847872+jcroomgitr@users.noreply.github.com>
2019-01-27 21:31:29 -0600

    Initial deployment

---
+++ precache-manifest.288fcdc3d31f84ddd836cc1eb23445c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8abffff897efa4abfad2",
+    "url": "/shopping-app/static/js/runtime~main.8abffff8.js"
+  },
+  {
+    "revision": "612b69e949e7aae57c09",
+    "url": "/shopping-app/static/js/main.612b69e9.chunk.js"
+  },
+  {
+    "revision": "199f44c5d3d58d9645f7",
+    "url": "/shopping-app/static/js/1.199f44c5.chunk.js"
+  },
+  {
+    "revision": "612b69e949e7aae57c09",
+    "url": "/shopping-app/static/css/main.7c552e8e.chunk.css"
+  },
+  {
+    "revision": "199f44c5d3d58d9645f7",
+    "url": "/shopping-app/static/css/1.45436986.chunk.css"
+  },
+  {
+    "revision": "24af60c9c081612c77c9e6010839ae11",
+    "url": "/shopping-app/index.html"
+  }
+];
